51ef5827e98ffc3c3017ea512b234016f96c5876
samet <samet.mollaoglu85@gmail.com>
2023-11-02 11:12:25 +0300

    project initialize added

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'package:architecture_template/feature/home/view/home_view.dart';
+import 'package:architecture_template/product/init/application_initialize.dart';
 import 'package:architecture_template/product/init/product_localization.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
+  await ApplicationInitialize().make();
 
-  runApp(ProductLocalization(child: const MyApp()));
+  runApp(ProductLocalization(child: const _MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _MyApp extends StatelessWidget {
+  const _MyApp();
 
   @override
   Widget build(BuildContext context) {
